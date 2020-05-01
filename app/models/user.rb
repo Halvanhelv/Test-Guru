a0a3674b50061c50102fd4904d8bc32889bbec66
SynthesisOne <developerfromrussia@gmail.com>
2020-05-02 00:10:25 +0300

    lesson_3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  def user_results(level)
+    Test.joins('join results on tests.id = results.test_id')
+        .where('results.user_id = ? and tests.level = ?', self.id, level)
+  end
 end
