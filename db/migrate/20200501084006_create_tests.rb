332f281c48ba73868f164db3e980d57d925e1f69
SynthesisOne <developerfromrussia@gmail.com>
2020-05-01 19:35:38 +0300

    after_rewiew

--- db/migrate/20200501084006_create_tests.rb
+++ db/migrate/20200501084006_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 0
-      t.references :categories, foreign_key: true, null: false
+      t.references :category, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,49 +12,45 @@
 
 ActiveRecord::Schema.define(version: 2020_05_01_084340) do
 
-  create_table 'answers', force: :cascade do |t|
-    t.string 'body', null: false
-    t.integer 'questions_id', null: false
-    t.boolean 'correct', default: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['questions_id'], name: 'index_answers_on_questions_id'
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'title', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'body', null: false
-    t.integer 'test_id', null: false
-    t.integer 'tests_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['tests_id'], name: 'index_questions_on_tests_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table 'tests', force: :cascade do |t|
-    t.string 'title', null: false
-    t.integer 'level', default: 0
-    t.integer 'categories_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['categories_id'], name: 'index_tests_on_categories_id'
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 0
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'login', null: false
-    t.string 'email', null: false
-    t.string 'password', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "login", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'answers', 'questions', column: 'questions_id'
-  add_foreign_key 'questions', 'tests', column: 'tests_id'
-  add_foreign_key 'tests', 'categories', column: 'categories_id'
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
