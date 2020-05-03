1ea54ddfe249004eeaf499023b85babef2d75b10
SynthesisOne <developerfromrussia@gmail.com>
2020-05-03 03:06:57 +0300

    fix_2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # has_and_belongs_to_many :tests
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id'
   def user_results(level)
     Test.joins('join results on tests.id = results.test_id')
         .where('results.user_id = ? and tests.level = ?', self.id, level)
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Dmitry', login: 'dmitry_1488', password: 'qwerty',email: 'asdasd@gmail')
+user = User.create(name: 'Dmitry', login: 'dmitry_1488', password: 'qwerty',email: 'asdasd@gmail')
 sport = Category.create(title: 'Sport')
 cars = Category.create(title: 'Cars')
 it = Category.create(title: 'IT')
 
-first_test = Test.create(title: 'Soccer', category_id: sport.id)
-second_test = Test.create(title: 'Buscet', category_id: cars.id)
-third_test = Test.create(title: 'Lada', category_id: cars.id)
-fourth_test = Test.create(title: 'Ruby', category_id: it.id)
-fifth_test = Test.create(title: 'PHP', category_id: it.id)
+first_test = Test.create(title: 'Soccer', category_id: sport.id,author_id:user.id)
+second_test = Test.create(title: 'Buscet', category_id: cars.id,author_id:user.id)
+third_test = Test.create(title: 'Lada', category_id: cars.id,author_id:user.id)
+fourth_test = Test.create(title: 'Ruby', category_id: it.id,author_id:user.id)
+fifth_test = Test.create(title: 'PHP', category_id: it.id,author_id:user.id)
 
 first_question = Question.create(body: 'Football in the usa', test_id: first_test.id)
 second_question = Question.create(body: 'Where is Lada produced?', test_id: second_test.id)
