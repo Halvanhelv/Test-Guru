a25030fe8384f339ed11cae601c67524c7885dcc
SynthesisOne <developerfromrussia@gmail.com>
2020-05-03 17:18:51 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_205650) do
+ActiveRecord::Schema.define(version: 2020_05_03_141536) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 2020_05_02_205650) do
     t.string "title", null: false
     t.integer "level", default: 0
     t.integer "category_id", null: false
-    t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
