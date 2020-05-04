473f08f8e13a27b411e57b89b1d6aaed37973d0c
SynthesisOne <developerfromrussia@gmail.com>
2020-05-04 04:43:09 +0300

    lesson_5

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # has_and_belongs_to_many :tests
   has_many :tests_users #цепляет  все строки из tests_users
   has_many :tests, through: :tests_users # а это уже позволяет по по пойманным строкам искать нужное в таблице users
-  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id'
+  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id' #вместо :tests_author можно указать что будет удобнее
+  validates :email, presence: true
 
-  def user_results(level)
-    Test.joins('join results on tests.id = results.test_id')
-        .where('results.user_id = ? and tests.level = ?', self.id, level)
+
+  scope :sort_level, ->(level) { joins(:tests) .where(tests: { level: level }) }
+
+
+  def self.user_results(level)
+    sort_level(level)
   end
 end
