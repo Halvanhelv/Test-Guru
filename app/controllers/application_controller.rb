d7beb41113ec4c9353d06d896376b7d565a53b7a
SynthesisOne <developerfromrussia@gmail.com>
2020-05-06 09:45:20 +0300

    lesson_7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protected
+
+  def test_params
+    params.require(:test).permit(:title, :category_id, :level, :author_id)
+  end
+
+
+
+
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+
 end
