9d99feae69c94e4db6b55802d81405207b411870
SynthesisOne <developerfromrussia@gmail.com>
2020-05-23 00:19:30 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_production"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'arcane-cliffs-55701.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'test-guru-gagiev.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   host = 'https://arcane-cliffs-55701.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
